abe2de7d6da90a090801740d857c2f02e6c1e4c6
jtingey <j.tingey.16@ucl.ac.uk>
2018-09-06 14:09:22 +0100

    Added PID to main code base, use new macro for easy running

--- include/WCSimPID.hh
+++ include/WCSimPID.hh
@@ -6,6 +6,8 @@
  */
 
 #include "TObject.h"
+#include "TTree.h"
+#include "TChain.h"
 
 #include <iostream>
 #include <cstdlib>
@@ -18,7 +20,53 @@ class WCSimPID: public TObject {
 		WCSimPID();
 		~WCSimPID();
 
-		void Echo();
+		void Train();
+		void Read();
+
+		void SetTrainingDirs(std::string elTrainDir, std::string muTrainDir, std::string ncTrainDir) {
+			fElTrainDir = elTrainDir;
+			fMuTrainDir = muTrainDir;
+			fNcTrainDir = ncTrainDir;
+		}
+		void PrintTrainingDirs() {
+			std::cout << "Electron CCQE Training Dir -> " << fElTrainDir << std::endl;
+			std::cout << "Muon CCQE Training Dir -> " << fMuTrainDir << std::endl;
+			std::cout << "NC Training Dir -> " << fNcTrainDir << std::endl;
+		}
+
+		void SetReadingDirs(std::string elAllDir, std::string muAllDir) {
+			fElAllDir = elAllDir;
+			fMuAllDir = muAllDir;
+		}
+		void PrintReadingDirs() {
+			std::cout << "Electron All Reading Dir -> " << fElAllDir << std::endl;
+			std::cout << "Muon All Reading Dir -> " << fMuAllDir << std::endl;
+		}
+
+	private:
+
+		// General Methods
+		void MakeCombinedFile(std::string trainDir, const char * trainingFileName);
+
+		// Training Methods
+		void MakeTrainingFile(std::string trainDir, const char * trainingFileName);
+		void MakeTrainingFile_NCSelect(std::string trainDir, const char * trainingFileName);
+
+		void TrainTMVA_electronMuonCCQE(const char * sigFileName, const char * bkgFileName);
+		void TrainTMVA_electronCCQEvsNC(const char * sigFileName, const char * bkgFileName);
+
+		void MakePlots(const char * sigFileName, const char * bkgFileName);
+
+		// Reading Methods
+		void ReadToFile(std::string readDir, const char * readFileName);
+		void ScanCuts(const char * elAllFile, const char * muAllFile);
+
+		std::string fElTrainDir;
+		std::string fMuTrainDir;
+		std::string fNcTrainDir;
+
+		std::string fElAllDir;
+		std::string fMuAllDir;
 
 		ClassDef(WCSimPID,1);
 };
